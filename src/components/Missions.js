f62ea05835b8f482babddc2d9d7cdc402c1b32b7
roger-mont <roger_galvin@hotmail.com>
2023-01-24 16:06:57 -0400

    requisito 9 e 10 completos

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 export default class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((e) => (
+          <MissionCard
+            key={ e.name }
+            name={ e.name }
+            year={ e.year }
+            country={ e.country }
+            destination={ e.destination }
+          />
+        ))}
       </div>
     );
   }
