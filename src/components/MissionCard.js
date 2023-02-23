d64843381bdca3d95ff79e68a0416382998fd295
roger-mont <roger_galvin@hotmail.com>
2023-02-23 14:07:58 -0400

    customizacao finalizada

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import '../styles/MissionCard.css';
 
 export default class MissionCard extends Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <p data-testid="mission-name">
+      <div
+        className="MissionCardContainer"
+        data-testid="mission-card"
+      >
+        <p
+          className="mission-name"
+          data-testid="mission-name"
+        >
           {name}
         </p>
-        <p data-testid="mission-year">
+        <p
+          className="mission-year"
+          data-testid="mission-year"
+        >
           {year}
         </p>
         <p data-testid="mission-country">
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,21 +2,28 @@ import React, { Component } from 'react';
 import Title from './Title';
 import missions from '../data/missions';
 import MissionCard from './MissionCard';
+import '../styles/Missions.css';
 
 export default class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div
+        data-testid="missions"
+      >
         <Title headline="Missões" />
-        {missions.map((e) => (
-          <MissionCard
-            key={ e.name }
-            name={ e.name }
-            year={ e.year }
-            country={ e.country }
-            destination={ e.destination }
-          />
-        ))}
+        <div
+          className="missions"
+        >
+          {missions.map((e) => (
+            <MissionCard
+              key={ e.name }
+              name={ e.name }
+              year={ e.year }
+              country={ e.country }
+              destination={ e.destination }
+            />
+          ))}
+        </div>
       </div>
     );
   }
